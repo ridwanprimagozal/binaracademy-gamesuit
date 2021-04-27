0fefdb703e01907bf0e2a61375ee1a14a593beed
ridwanprimagozal <ridwanprimagozzal@gmail.com>
2021-04-28 00:07:51 +0700

    Progress 27 April 2020

---
+++ main/kotlin/SuitPemainSatu.kt
@@ -0,0 +1,19 @@
+import java.lang.reflect.Constructor
+
+class SuitPemainSatu() {
+    fun suitHasilPemain (pemainSatu:String, pemainDua:String) {
+
+        val gunting : String = "gunting"
+        val batu : String = "batu"
+        val kertas : String = "kertas"
+
+        if(pemainSatu == "gunting" && pemainDua == "kertas"  )
+        {
+            println("Pemain 1 Menang")
+        }
+
+
+
+
+    }
+}
--- main/kotlin/main.kt
+++ main/kotlin/main.kt
@@ -1,3 +1,24 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("""     
+        
+        =============================================
+        Aplikasi Penghitung Rata Rata Nilai Mahasiswa
+        =============================================
+        
+    """.trimIndent())
+
+
+
+    println("1. Masukan Pemain 1: ")
+    val pemainSatu = readLine()
+
+
+    println("2. Masukan Pemain 2: ")
+    val pemainDua = readLine()
+
+    val suitPemaninSatu = SuitPemainSatu()
+
+
 }
+
+
